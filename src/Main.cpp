dadd2bda0777fa3c7f1359d12902b491baac5e6f
VitorRibeiroCustodio <vitorribeiro221196@gmail.com>
2020-07-18 21:22:14 -0300

    Remove files

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,15 +1,5 @@
-#include "Engine.h"
 
 int main() {
-  auto game = Engine::GetInstance();
 
-  game->Init();
-  while (game->IsRunning())
-  {
-   game->Events();
-   game->Update();
-   game->Render();
-  }
-  game->Clean();
   return 0;
 }
